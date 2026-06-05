c3756d731b4fe06c1b4d1b6fe8bfa0b8b95d7265
thomas <tomcilia001@gmail.com>
2026-06-05 17:29:22 +0200

    Add files via upload

--- apple basket homework/code.py
+++ apple basket homework/code.py
@@ -3,6 +3,9 @@ import pgzrun
 
 WIDTH=800
 HEIGHT=600
+TITLE='collect the apples'
+
+gameover=False
 
 basket=Actor("basket")
 basket.pos=(400, 550)
@@ -12,27 +15,37 @@ score=0
 
 def draw():
     screen.fill("white")
-    basket.draw()
-    apple.draw()
-    screen.draw.text("Score: " + str(score), (10, 10), color="black")
+    if not gameover:
+        basket.draw()
+        apple.draw()
+        screen.draw.text("Score: " + str(score), (10, 10), color="black")
+    else:
+        screen.draw.text("youre final score is :" + str(score), center=(WIDTH/2,HEIGHT/2), color="black")
     
 
 def update():
     global score 
-    apple.y+=5
-    if apple.colliderect(basket):
-        score+=1
-        apple.y=0
-        apple.x=random.randint(50, 750)
-    if apple.y>HEIGHT:
-        score=0
-        apple.y=0
-        apple.x=random.randint(50, 750)
+    if not gameover:
+        apple.y+=5
+        if apple.colliderect(basket):
+            score+=1
+            apple.y=0
+            apple.x=random.randint(50, 750)
+        if apple.y>HEIGHT:
+            score=0
+            apple.y=0
+            apple.x=random.randint(50, 750)
 
-    if keyboard.left:
-        basket.x-=5
-    if keyboard.right:
-        basket.x+=5
+        if keyboard.left:
+            basket.x-=5
+        if keyboard.right:
+            basket.x+=5
 
+def timeup ():
+    global gameover 
 
-pgzrun.go()  
+    gameover=True
+
+clock.schedule(timeup,60)
+pgzrun.go()   
+
