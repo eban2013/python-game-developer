02ba5c1f48e3b5760f1d2de03bc759c8666684f5
thomas <tomcilia001@gmail.com>
2026-06-05 17:56:07 +0200

    Add files via upload

--- satalite_game/code.py
+++ satalite_game/code.py
@@ -4,9 +4,29 @@ import random
 WIDTH=800
 HIGHT=600
 
+satellite_no=8
+
+satellites=[]
+for i in range(satellite_no):
+    satellite=Actor("satellite")
+
+    satellite.x=random.randint(50,WIDTH-50)
+    satellite.y=random.randint(50,HIGHT-50)
+
+    satellites.append(satellite)
+    
+print(satellites)
+
+
+
 def draw():
 
     screen.blit("space",(0,0))
+    for sat in satellites:
+        sat.draw()
+
+
+
 
 
 pgzrun.go()
