4ff64e1dd848643fd3743048301bf8b48e5df08f
thomas <tomcilia001@gmail.com>
2026-06-12 18:00:23 +0200

    Add files via upload

--- satalite_game/code.py
+++ satalite_game/code.py
@@ -4,10 +4,18 @@ import random
 WIDTH=800
 HIGHT=600
 
+sat_order=0
 satellite_no=8
+
+current_sat=0
+
+lines=[]
 
 satellites=[]
 for i in range(satellite_no):
+
+
+
     satellite=Actor("satellite")
 
     satellite.x=random.randint(50,WIDTH-50)
@@ -20,11 +28,32 @@ print(satellites)
 
 
 def draw():
+    sat_order=1
 
     screen.blit("space",(0,0))
     for sat in satellites:
         sat.draw()
+        screen.draw.text (str(sat_order),(sat.x-30,sat.y-30))
+        sat_order=sat_order+1
 
+    for line in lines:
+        screen.draw.line(line[0],line[1],'white')
+
+
+
+def on_mouse_down(pos):
+    global current_sat,lines
+    if satellites[current_sat].collidepoint(pos):
+        if current_sat>0 :
+            start=satellites[current_sat].pos
+            end=satellites[current_sat-1].pos
+            lines.append([start,end])
+    
+        current_sat=current_sat+1
+    else:
+        current_sat=0
+        lines=[]
+    
 
 
 
