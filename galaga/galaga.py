5e5ddcf08356273bdafb15d0c3fb42d9286ffb06
thomas <tomcilia001@gmail.com>
2026-08-07 17:56:32 +0200

    Add files via upload

--- galaga/galaga.py
+++ galaga/galaga.py
@@ -8,9 +8,31 @@ galaga.pos=(WIDTH/2,HEIGHT-50)
 bullets=[]
 
 
+
+enemies=[]
+y=50
+for i in range (3):
+    row=[]
+    x=50
+    for q in range(4):
+        enemy=Actor('enemy')
+        enemy.x=x
+        enemy.y=y
+        row.append(enemy)
+        x=x+60
+    enemies.append(row)
+    y=y+60
+
 def draw ():
     screen.blit('space',(0,0))
     galaga.draw()
+
+    for bullet in bullets:
+        bullet.draw()
+
+    for row in enemies:
+        for enemy in row :
+            enemy.draw()
 
 
 def update():
@@ -19,10 +41,25 @@ def update():
 
     if keyboard.d :
         galaga.x=galaga.x+3
-    
+
+    for bullet in bullets:
+        bullet.y=bullet.y-5
+        if bullet.y<0:
+            bullets.remove(bullet)
+
+    for i in range (3):
+        for enemy in enemies[i]:
+            for bullet in bullets:
+                if enemy.colliderect(bullet):
+                    enemies[i].remove(enemy)
+                    bullets.remove(bullet)
+
+
+
 def on_key_down(key):
     if key==keys.SPACE:
         bullet=Actor('bullet')
+        bullet.pos=galaga.pos
         bullets.append(bullet)
 
     print(bullets)
